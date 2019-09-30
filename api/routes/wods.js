cc651b719170fb686e0a60b27658176ad9a4042a
Skirmantas-Klizas <skirmis.klizas@gmail.com>
2019-09-30 14:14:21 +0300

    wods changes

--- api/routes/wods.js
+++ api/routes/wods.js
@@ -7,12 +7,12 @@ const Wod = require('../models/wod');
 router.get('/', async (req, res) => {
     try {
         const wods = await Wod.find().select('-__v');
-        res.json({
+        res.status(200).json({
             count: wods.length,
             wods: wods
         });
-    } catch (error) {
-        res.json({ message: error });
+    } catch {
+        res.status(500).json({ message: error });
     }
 });
 
@@ -62,31 +62,30 @@ router.get('/', async (req, res) => {
 // });
 
 router.post('/', async (req, res) => {
+    const wod = new Wod({
+        _id: new mongoose.Types.ObjectId(),
+        title: req.body.title,
+        type: req.body.type,
+        difficulty: req.body.difficulty,
+        workout: req.body.workout,
+        example: req.body.example
+    });
     try {
-        const wod = new Wod({
-            _id: new mongoose.Types.ObjectId(),
-            title: req.body.title,
-            type: req.body.type,
-            difficulty: req.body.difficulty,
-            workout: req.body.workout,
-            example: req.body.example
-        });
         await wod.save();
-        res.json({ wod: wod });
-    } catch (error) {
-        res.json({ message: error });
+        res.status(201).json({ wod: wod });
+    } catch {
+        res.status(500).json({ message: error });
     }
 });
 
 router.get('/:wodId', async (req, res) => {
     const id = req.params.wodId;
     try {
-        // const id = req.params.wodId;
         const wod = await Wod.findById(id).select('-__v');
 
-        res.json({ wod: wod });
+        res.status(200).json({ wod: wod });
     } catch (error) {
-        res.json({ message: error });
+        res.status(500).json({ message: error });
     }
 });
 
@@ -110,36 +109,34 @@ router.get('/:wodId', async (req, res) => {
 //         });
 // });
 
-router.patch('/:wodId', (req, res, next) => {
-    const id = req.params.wodId;
-    const updateOps = {};
-    for (const ops of req.body) {
-        updateOps[ops.propName] = ops.value;
-    }
-    Wod.update({ _id: id }, { $set: updateOps })
-        .exec()
-        .then(result => {
-            console.log(result);
-            res.status(200).json(result);
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json({
-                error: error
-            });
-        });
-});
+// router.patch('/:wodId', (req, res, next) => {
+//     const id = req.params.wodId;
+//     const updateOps = {};
+//     for (const ops of req.body) {
+//         updateOps[ops.propName] = ops.value;
+//     }
+//     Wod.update({ _id: id }, { $set: updateOps })
+//         .exec()
+//         .then(result => {
+//             console.log(result);
+//             res.status(200).json(result);
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             res.status(500).json({
+//                 error: error
+//             });
+//         });
+// });
 
 router.patch('/:wodId', async (req, res) => {
     const id = req.params.wodId;
-    const updateOps = {};
-    for (const ops of req.body) {
-        updateOps[ops.propName] = ops.value;
-    }
+    const set = req.body;
     try {
-        await Wod.update({ _id: id }, { $set: updateOps });
-    } catch (error) {
-        res.json({ message: error });
+        await Wod.updateOne({ _id: id }, set);
+        res.status(200).json({ id: id });
+    } catch {
+        res.status(404).json({ message: error });
     }
 });
 
